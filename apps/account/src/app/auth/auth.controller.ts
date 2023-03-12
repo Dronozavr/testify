87452950a1a7fe5b3b216c04c2b2bf5f47a2300e
Dronozavr <AndriyKuf@gmail.com>
2023-03-12 13:00:45 +0200

    created contracts for register user and get token

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,11 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { AccountLogin, AccountRegister } from '@testify/contracts';
 import { AuthService } from './auth.service';
-
-export class RegisterDto {
-    email: string;
-    password: string;
-    displayName: string;
-}
 @Controller('auth')
 export class AuthController {
     constructor(
@@ -13,12 +8,12 @@ export class AuthController {
     ) {}
 
     @Post('register')
-    async register(@Body() dto: RegisterDto) {
+    async register(@Body() dto: AccountRegister.Request): Promise<AccountRegister.Response> {
         return this.authService.register(dto);
     }
 
     @Post('login')
-    async login(@Body() { email, password }: Omit<RegisterDto, 'displayName'>) {
+    async login(@Body() { email, password }: AccountLogin.Request): Promise<AccountLogin.Response> {
        const { id } = await this.authService.validateUser(email, password);
        return this.authService.login(id);
     }
