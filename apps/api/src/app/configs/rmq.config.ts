13b490a753f24ee45566b4584449619071dfa413
Dronozavr <AndriyKuf@gmail.com>
2023-03-13 23:25:53 +0200

    chore: added service api

---
+++ apps/api/src/app/configs/rmq.config.ts
@@ -0,0 +1,22 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { IRMQServiceAsyncOptions } from "nestjs-rmq";
+
+
+export const getRMQConfig = (): IRMQServiceAsyncOptions => ({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory(configService: ConfigService) {
+        return {
+            exchangeName: configService.get('AMQP_EXCHANGE') ?? '',
+            connections: [
+                {
+                    login: configService.get('AMQP_USER') ?? '',
+                    password: configService.get('AMQP_PASSWORD') ?? '',
+                    host: configService.get('AMQP_HOSTNAME') ?? '',
+                }
+            ],
+            prefetchCount: 32,
+            serviceName: 'testify-auth'
+        }
+    },
+});
